3bba3e865ea8bd1c8e6deb67d519337c8098ccf7
askorbezh <artyom.ireday@gmail.com>
2021-07-24 22:44:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let openBracket = 0;
+    let closeBracket = 0;
+
+    if (expr.indexOf('/ 0') + 1) throw new Error('TypeError: Division by zero.');
+
+    for(let i = 0; i < expr.length; i++) {
+        if (expr[i] === '(') openBracket++;
+        if (expr[i] === ')') closeBracket++;
+    }
+    if (openBracket !== closeBracket) throw new Error('ExpressionError: Brackets must be paired.');
+    return new Function (`return ${expr}`)();
 }
 
 module.exports = {
